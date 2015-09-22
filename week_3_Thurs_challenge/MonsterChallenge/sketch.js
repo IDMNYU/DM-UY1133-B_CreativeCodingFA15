1a7e2e77abf145b6c1968fa7ed8d41765c2e5d73
LadyK <static.katherine@gmail.com>
2015-09-22 12:01:48 -0400

    In-Class Challenge + P5.js boiler plate

---
+++ week_3_Thurs_challenge/MonsterChallenge/sketch.js
@@ -0,0 +1,37 @@
+
+
+// Declare all global variables (stays the same)
+// var width = 40;  etc
+
+// Setup does not change
+function setup() {
+  createCanvas(480, 270);  // you can change how big your sketch is in the browser
+}
+
+function draw() {
+ 
+}
+
+// Where should functions be placed?
+// You can define your functions anywhere in the code outside of setup() and draw().
+// However, the convention is to place your function definitions below draw().
+
+// A function 
+function move() {  // feel free to rename these functions
+  // Change the x location by speed
+
+}
+
+// A function to make the body
+// The key point of this challenge, is to be able to
+// pass in parameters to make the body unique, each time this 
+// function is called. Color, location, size and width are good things to work with
+function monsterBody() {  // feel free to rename these functions
+ 
+  
+}
+
+// A function 
+function display() { // feel free to rename these functions
+  
+}
